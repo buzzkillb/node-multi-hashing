03c976f7991871300c7315c1ce338cb2ec381182
Mike <zpoolsde@gmail.com>
2018-04-13 09:31:20 +0200

    sonoa update

--- sonoa.c
+++ sonoa.c
@@ -26,32 +26,32 @@ void sonoa_hash(const char* input, char* output, uint32_t len)
 	uint32_t hash[16];
 
         sph_echo512_init(&ctx_echo);
-        sph_echo512(&ctx_echo, input, 80);
-        sph_echo512_close(&ctx_echo, (void*) hash);
+        sph_echo512(&ctx_echo, input, len);
+        sph_echo512_close(&ctx_echo, hash);
 
         sph_skein512_init(&ctx_skein);
-        sph_skein512(&ctx_skein, (const void*) hash, 64);
-        sph_skein512_close(&ctx_skein, (void*) hash);
+        sph_skein512(&ctx_skein, hash, 64);
+        sph_skein512_close(&ctx_skein, hash);
 
         sph_fugue512_init(&ctx_fugue);
-        sph_fugue512(&ctx_fugue, (const void*) hash, 64);
-        sph_fugue512_close(&ctx_fugue, (void*) hash);
+        sph_fugue512(&ctx_fugue, hash, 64);
+        sph_fugue512_close(&ctx_fugue, hash);
 
         sph_gost512_init(&ctx_gost);
-        sph_gost512(&ctx_gost, (const void*) hash, 64);
-        sph_gost512_close(&ctx_gost, (void*) hash);
+        sph_gost512(&ctx_gost, hash, 64);
+        sph_gost512_close(&ctx_gost, hash);
 
         sph_bmw512_init(&ctx_bmw);
-        sph_bmw512(&ctx_bmw, (const void*) hash, 64);
-        sph_bmw512_close(&ctx_bmw, (void*) hash);
+        sph_bmw512(&ctx_bmw, hash, 64);
+        sph_bmw512_close(&ctx_bmw, hash);
 
         sph_jh512_init(&ctx_jh);
-        sph_jh512(&ctx_jh, (const void*) hash, 64);
-        sph_jh512_close(&ctx_jh, (void*) hash);
+        sph_jh512(&ctx_jh, hash, 64);
+        sph_jh512_close(&ctx_jh, hash);
 
         sph_keccak512_init(&ctx_keccak);
-        sph_keccak512(&ctx_keccak, (const void*) hash, 64);
-        sph_keccak512_close(&ctx_keccak, (void*) hash);
+        sph_keccak512(&ctx_keccak, hash, 64);
+        sph_keccak512_close(&ctx_keccak, hash);
 
 	memcpy(output, hash, 32);
 }
