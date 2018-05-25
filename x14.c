94827afa5571035dea739c845fc7a32650c7d9d8
Mike <zpoolsde@gmail.com>
2018-05-25 15:58:01 +0200

    test x14

--- x14.c
+++ x14.c
@@ -38,20 +38,20 @@ void x14_hash(const char* input, char* output, uint32_t len)
     sph_fugue512_context      ctx_fugue;
     sph_shabal512_context     ctx_shabal;
 
-    uint512 hash[17];
+    uint32_t hash[17];
 
 
 
     sph_blake512_init(&ctx_blake);
     sph_blake512 (&ctx_blake,  input, len);
-    sph_blake512_close(&ctx_blake,hash[0hash);
+    sph_blake512_close(&ctx_blake,hash);
     
     sph_bmw512_init(&ctx_bmw);
     sph_bmw512 (&ctx_bmw, hash, 64);
     sph_bmw512_close(&ctx_bmw, hash);
 
     sph_groestl512_init(&ctx_groestl);
-    sph_groestl512 (&ctx_groestl,hash, 64);
+    sph_groestl512 (&ctx_groestl, hash, 64);
     sph_groestl512_close(&ctx_groestl, hash);
 
     sph_skein512_init(&ctx_skein);
@@ -59,24 +59,24 @@ void x14_hash(const char* input, char* output, uint32_t len)
     sph_skein512_close(&ctx_skein, hash);
     
     sph_jh512_init(&ctx_jh);
-    sph_jh512 (&ctx_jh,  3hash, 64);
+    sph_jh512 (&ctx_jh,  hash, 64);
     sph_jh512_close(&ctx_jh, hash);
     
     sph_keccak512_init(&ctx_keccak);
     sph_keccak512 (&ctx_keccak, hash, 64);
-    sph_keccak512_close(&ctx_keccak,hash);
+    sph_keccak512_close(&ctx_keccak, hash);
 
     sph_luffa512_init(&ctx_luffa);
     sph_luffa512 (&ctx_luffa, hash, 64);
-    sph_luffa512_close(&ctx_luffa,hash);
+    sph_luffa512_close(&ctx_luffa, hash);
     
     sph_cubehash512_init(&ctx_cubehash);
     sph_cubehash512 (&ctx_cubehash, hash, 64);
-    sph_cubehash512_close(&ctx_cubehash,hash;
+    sph_cubehash512_close(&ctx_cubehash, hash);
     
     sph_shavite512_init(&ctx_shavite);
     sph_shavite512(&ctx_shavite, hash, 64);
-    sph_shavite512_close(&ctx_shavite,hash);
+    sph_shavite512_close(&ctx_shavite, hash);
         
     sph_simd512_init(&ctx_simd);
     sph_simd512 (&ctx_simd, hash, 64);
@@ -84,15 +84,15 @@ void x14_hash(const char* input, char* output, uint32_t len)
 
     sph_echo512_init(&ctx_echo);
     sph_echo512 (&ctx_echo, hash, 64);
-    sph_echo512_close(&ctx_echo,hash);
+    sph_echo512_close(&ctx_echo, hash);
 
     sph_hamsi512_init(&ctx_hamsi);
     sph_hamsi512 (&ctx_hamsi, hash, 64);
-    sph_hamsi512_close(&ctx_hamsi,hash);
+    sph_hamsi512_close(&ctx_hamsi, hash);
 
     sph_fugue512_init(&ctx_fugue);
     sph_fugue512 (&ctx_fugue, hash, 64);
-    sph_fugue512_close(&ctx_fugue,hash);
+    sph_fugue512_close(&ctx_fugue, hash);
 
     sph_shabal512_init(&ctx_shabal);
     sph_shabal512 (&ctx_shabal, hash, 64);
